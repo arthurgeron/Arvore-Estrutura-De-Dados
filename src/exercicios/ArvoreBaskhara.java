b8a075a52d8e57a9cb580a629e902123710a8ea6
Arthur Geron <arthcronos@hotmail.com>
2015-03-16 20:38:32 -0300

    Método de criação de árvore atualizado

--- src/exercicios/ArvoreBaskhara.java
+++ src/exercicios/ArvoreBaskhara.java
@@ -1,5 +1,6 @@
 package exercicios;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -53,188 +54,52 @@ public class ArvoreBaskhara {
         
     }
     //Preencher Nodulo
-    public static void CriarArvore(ArvoreBaskhara raiz, String[] numerosSeguidosDeOperandos)
-    {
-    	int contadorNodulo=0,contadorVetor=0;
-    	ArvoreBaskhara noduloAtual = raiz;
-    	while(contadorVetor+1<numerosSeguidosDeOperandos.length)
-    	{
-    		while(contadorNodulo<3)
-    		{
-    			contadorNodulo++;
-    			
-    			
-    			if(contadorNodulo==1)
-    			{
-    				
-    				//Regex para saber se o próximo elemento é um número ou operando
-    			
-    				Pattern pattern = Pattern.compile("[0-9]");
-    				Matcher matcher = pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]);
-    				if(matcher.matches())//Caso a próxima posição seja um número
-    				{
-    					if(noduloAtual.Number==null && pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]).matches()) 
-						{
-							noduloAtual.Number = numerosSeguidosDeOperandos[contadorVetor];
-							contadorVetor++;
-						}
-    					if(noduloAtual.NumberOperator==null && !pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]).matches()) 
-    					{
-    						noduloAtual.NumberOperator = numerosSeguidosDeOperandos[contadorVetor];
-    						contadorVetor++;
-    					}
-    					
-    				}
-    				else
-    				{
-    					if(contadorVetor+2<=numerosSeguidosDeOperandos.length){
-							if(noduloAtual.Number==null && noduloAtual.NumberOperator==null) {
-								noduloAtual.Number = numerosSeguidosDeOperandos[contadorVetor+1];
-								noduloAtual.NumberOperator = numerosSeguidosDeOperandos[contadorVetor];
-								contadorVetor += 2;
-							}
-							else if(noduloAtual.NumberOperator==null && !pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]).matches())
-							{	
-								noduloAtual.NumberOperator = numerosSeguidosDeOperandos[contadorVetor];
-								contadorVetor++;
-							}
-    					}
-    				}
-    				if(contadorVetor + 7 <= numerosSeguidosDeOperandos.length)
-    				{
-	    				
-    					matcher = pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]);
-	    				if(matcher.matches())//Caso a próxima posição seja um número
-	    				{
-	    					ArvoreBaskhara novoNoduloDireito = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor],numerosSeguidosDeOperandos[contadorVetor+1],new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+2]),numerosSeguidosDeOperandos[contadorVetor+3],new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+4]));
-	    					contadorVetor += 5;
-	    					noduloAtual.LeftRightBinaryOperator = numerosSeguidosDeOperandos[contadorVetor];
-	    					contadorVetor++;
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor]);
-	    					contadorVetor ++;
-	    					noduloAtual.setNoduloDireito(novoNoduloDireito);
-	    					noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    				}
-	    				else // Caso a próxima posição seja um operando
-	    				{   //criar apenas um numero no esquerdo
-	    					ArvoreBaskhara novoNoduloDireito = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+1],numerosSeguidosDeOperandos[contadorVetor],new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+3]),numerosSeguidosDeOperandos[contadorVetor+2],new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+5]));
-	    					noduloAtual.LeftRightBinaryOperator = numerosSeguidosDeOperandos[contadorVetor+4];
-	    					contadorVetor += 6;
-	    					//Porque apenas 1 variável na criação do nódulo esquerdo?
-	    					//Porque esta árvore é ordenada, e portanto, irá sempre preencher o nódulo da direita completamente e irá preencher
-	    					//o nódulo da esquerda parcialmente, de forma que ele será o próximo nódulo a ser preenchido caso
-	    					// ainda tenham posições suficientes no vetor a serem lidas.
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor]);
-	    					contadorVetor++;
-	    					noduloAtual.setNoduloDireito(novoNoduloDireito);
-	    					noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    				}
-    				
-    				}
-    				else if(contadorVetor + 4 <= numerosSeguidosDeOperandos.length)	
-        			{
-        				
-    					matcher = pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]);
-	    				if(matcher.matches())//Caso a próxima posição seja um número
-	    				{
-	    					ArvoreBaskhara novoNoduloDireito = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor]);
-	    					noduloAtual.LeftRightBinaryOperator = numerosSeguidosDeOperandos[contadorVetor+1];
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+2]);
-	    					novoNoduloEsquerdo.NumberOperator = numerosSeguidosDeOperandos[contadorVetor+3];
-	    					contadorVetor += 4;
-	    					noduloAtual.setNoduloDireito(novoNoduloDireito);
-	    					noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    					
-	    				}
-	    				else // Caso a próxima posição seja um operando
-	    				{   
-	    					ArvoreBaskhara novoNoduloDireito = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+1]);
-	    					noduloAtual.LeftRightBinaryOperator = numerosSeguidosDeOperandos[contadorVetor];
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+3]);
-	    					novoNoduloEsquerdo.NumberOperator = numerosSeguidosDeOperandos[contadorVetor+2];
-	    					contadorVetor += 4;
-	    					noduloAtual.setNoduloDireito(novoNoduloDireito);
-	    					noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    				}
-        			}
-    				else if(contadorVetor + 2 <= numerosSeguidosDeOperandos.length)
-    				{
-    					matcher = pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]);
-	    				if(matcher.matches())//Caso a próxima posição seja um número
-	    				{
-	    					try{
-	    						ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+1]);
-	    						contadorVetor++;
-	    						try{
-		    						novoNoduloEsquerdo.NumberOperator = numerosSeguidosDeOperandos[contadorVetor+1];
-		    						contadorVetor++;
-		    					}
-		    					catch(Exception ex)
-		    					{
-		    						
-		    					}
-	    						if(noduloAtual.esquerda!=null){ noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);} else{noduloAtual.setNoduloDireito(novoNoduloEsquerdo); };
-		    					noduloAtual = novoNoduloEsquerdo;
-		    					break;
-	    					}
-	    					catch(Exception ex)
-	    					{
-	    					
-	    					}
-    						
-	    					
-	    				}
-	    				else // Caso a próxima posição seja um operando
-	    				{   
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+2]);
-	    					novoNoduloEsquerdo.NumberOperator = numerosSeguidosDeOperandos[contadorVetor+1];
-	    					contadorVetor += 2;
-	    					if(noduloAtual.esquerda!=null){ noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);} else{noduloAtual.setNoduloDireito(novoNoduloEsquerdo); };
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    				}
-    				}
-    				else if(contadorVetor + 1 <= numerosSeguidosDeOperandos.length)
-    				{
-    					matcher = pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]);
-	    				if(matcher.matches())//Caso a próxima posição seja um número
-	    				{
-	    					ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+1]);
-	    					contadorVetor++;
-	    					if(noduloAtual.esquerda!=null){ noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);} else{noduloAtual.setNoduloDireito(novoNoduloEsquerdo); };
-	    					noduloAtual = novoNoduloEsquerdo;
-	    					break;
-	    				}
-	    				else
-	    				{
-	    					contadorVetor++;
-	    					break;
-	    				}
-    					
-    				}
-    				else
-    				{
-    					if(pattern.matcher(numerosSeguidosDeOperandos[contadorVetor]).matches())
-    					{
-    						ArvoreBaskhara novoNoduloEsquerdo = new ArvoreBaskhara(numerosSeguidosDeOperandos[contadorVetor+1]);
-    						contadorVetor++;
-    						if(noduloAtual.esquerda!=null){ if(noduloAtual.esquerda!=null){ noduloAtual.setNoduloEsquerdo(novoNoduloEsquerdo);} else{noduloAtual.setNoduloDireito(novoNoduloEsquerdo); };} else{noduloAtual.setNoduloDireito(novoNoduloEsquerdo); };
-    					}
-    				}
-    				
-    			}
-    		}
-    		contadorNodulo = 0;
-    	}
-    }
+    public static void CriarArvore(ArvoreBaskhara raiz, ArrayList<String> vetorOperacoes)
+	{
+		Pattern pattern = Pattern.compile("[0-9]");
+		ArvoreBaskhara noduloAtual = raiz;
+		for(String operacaoOuNumero : vetorOperacoes)
+		{
+			if(noduloAtual.Number == null)
+			{
+				if(pattern.matcher(operacaoOuNumero).matches())
+				{
+					noduloAtual.Number = operacaoOuNumero;
+				}
+			}
+			else if ( noduloAtual.NumberOperator == null)
+			{
+				if(!pattern.matcher(operacaoOuNumero).matches())
+				{
+					noduloAtual.NumberOperator = operacaoOuNumero;
+				}
+			}
+			else if(pattern.matcher(operacaoOuNumero).matches())
+			{
+					if(noduloAtual.direita==null)
+					{
+						noduloAtual.direita = new ArvoreBaskhara(operacaoOuNumero);
+					}
+					else
+					{
+						noduloAtual.esquerda = new ArvoreBaskhara(operacaoOuNumero);
+					}
+					if(noduloAtual.direita!=null && noduloAtual.esquerda!=null && noduloAtual.LeftRightBinaryOperator!=null)
+					{
+						noduloAtual = noduloAtual.esquerda;
+					}
+			}
+			else // Caso seja um símbolo
+			{
+				if(noduloAtual.LeftRightBinaryOperator == null)
+				{
+					noduloAtual.LeftRightBinaryOperator = operacaoOuNumero;
+				}
+			}
+			
+		}
+		
+	}
     //Percorre a arvore
     public static String PercorrerArvereBaskhara(ArvoreBaskhara arvereBaskhara)
     {
